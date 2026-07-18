8339d1f5c5416b5bb3ce876217a20c2fd500c93d
yaswanthbondigala <bondigalayaswanth@gmail.com>
2026-07-18 14:04:20 +0530

    initial commit

---
+++ operators/Relational_operators.py
@@ -0,0 +1,10 @@
+#relational operators
+
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+print("a>=b",a>=b)
+print("a<=b",a<=b)
+print("a>b",a>b)
+print("a<b",a<b)
+print("a==b",a==b)
+print("a!=b",a!=b)
