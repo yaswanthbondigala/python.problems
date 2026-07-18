362d0535ccdc49d05460ce28c3f78b79f91f9c26
yaswanthbondigala <bondigalayaswanth@gmail.com>
2026-07-18 14:03:19 +0530

    updated program

--- operators/Arithmetic_operators.py
+++ operators/Arithmetic_operators.py
@@ -1,32 +1,9 @@
-'''
-a=10
-b=20
-c=a+b
-print(c)
-'''
-
-'''
-a=20
-b=15
-c=a-b
-print(a)
-'''
-
-'''
-a=20
-b=10
-c=a//b
-print(c)
-'''
-
-'''
-a=10
-b=8
-c=a*b
-print(c)
-'''
-
-a=10
-b=4
-c=a**b
-print(c)
+# arithmetic operators
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a//b=",a//b)
+print("a%b=",a%b)
